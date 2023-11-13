288f6885abd64fdf4a7f2089c9e671f0c5140845
rafroes1 <rafafroes@outlook.com>
2023-11-13 10:43:28 -0300

    Initial Commit

---
+++ DesignPatterns/DesignPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternsApp.swift
+//  DesignPatterns
+//
+//  Created by Rafael Carvalho on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
